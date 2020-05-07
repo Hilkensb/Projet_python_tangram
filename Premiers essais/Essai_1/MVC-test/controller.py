8740492fe3b455541a0cb231c08da17251385d77
Twomens <55756182+Twomens@users.noreply.github.com>
2020-05-07 16:42:14 +0200

    x

--- Premiers essais/Essai_1/MVC-test/controller.py
+++ Premiers essais/Essai_1/MVC-test/controller.py
@@ -1,27 +1,35 @@
 import model
 import vue
 
+import numpy
+import matplotlib.path as mpltPath
+
 # functio to transform the coordinates of a shape to be displayed at a certain
 # locolisation
 # input : shape[[x,y]...],
 # top left hand corner coordinates where it's displayed [x,y]
 # output : new shape with offset
 def offsetShape (shape, offset) :
-    for i in range(shape) :
+    for i in range(0, len(shape)) :
         shape[i][0] += offset[0]
         shape[i][1] += offset[1]
+    return shape
 
-def shapeFits (shape, editedPatron):
+def shapeFits (shape) :
+    PATRONBIS = [(0,0),(100,0),(100,100),(200,100),(200,200),(100,200),(0,200),(0,100)]
+    SHAPE_1BIS = [(0,0),(100,0),(100,100),(0,100)]
+    path = mpltPath.Path(PATRONBIS)
+    print( path.contains_points(SHAPE_1BIS) )
     return 0
 
 #_________________________________________________TEST_________________________________________________
-shapePtaron = model.PATRON
-offsetShape(shapePtaron, [1000,1000])
-print(shapePtaron)
+
 
 #_________________________________________________DISPLAY_________________________________________________
 
+shapeFits(1)
 vue.affiche()
+
 
 #_________________________________________________RULES_________________________________________________
 
--- Premiers essais/Essai_1/MVC-test/vue.py
+++ Premiers essais/Essai_1/MVC-test/vue.py
@@ -2,6 +2,12 @@ import model
 import numpy
 import cv2
 
+def offsetShape (shape, offset) :
+    for i in range(0, len(shape)) :
+        shape[i][0] += offset[0]
+        shape[i][1] += offset[1]
+    return shape
+
 def affiche():
     display = numpy.zeros((model.SCREEN_LENGTH,model.SCREEN_WIDTH,3), numpy.uint8)
     
@@ -14,22 +20,22 @@ def affiche():
         cv2.putText(display,(str)(i*100)  ,(i*100-20,85), cv2.FONT_HERSHEY_SIMPLEX, 1,model.WHITE,2,cv2.LINE_AA)
     
     #Shape_1
-    pts = numpy.array(model.SHAPE_1, numpy.int32)
+    pts = numpy.array(offsetShape(model.SHAPE_1,[100,100]), numpy.int32)
     cv2.fillPoly(display, [pts], model.BLUE)
     display = cv2.polylines(display,[pts],True,model.WHITE,3)
     
     #Shape_2
-    pts = numpy.array(model.SHAPE_2, numpy.int32)
+    pts = numpy.array(offsetShape(model.SHAPE_2,[200,100]), numpy.int32)
     cv2.fillPoly(display, [pts], model.BLUE)
     display = cv2.polylines(display,[pts],True,model.WHITE,3)
     
     #Shape_3
-    pts = numpy.array(model.SHAPE_3, numpy.int32)
+    pts = numpy.array(offsetShape(model.SHAPE_3,[300,100]), numpy.int32)
     cv2.fillPoly(display, [pts], model.BLUE)
     display = cv2.polylines(display,[pts],True,model.WHITE,3)
     
     #Patron
-    pts = numpy.array(model.PATRON, numpy.int32)
+    pts = numpy.array(offsetShape(model.PATRON,[600,100]), numpy.int32)
     cv2.fillPoly(display, [pts], model.RED)
     display = cv2.polylines(display,[pts],True,model.WHITE,3)
     
