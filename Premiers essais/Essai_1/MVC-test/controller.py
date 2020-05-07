5e786720b4151767627b5c5aa5eb5c9a3906a367
Twomens <55756182+Twomens@users.noreply.github.com>
2020-05-07 22:42:31 +0200

    fonction fitShape

--- Premiers essais/Essai_1/MVC-test/controller.py
+++ Premiers essais/Essai_1/MVC-test/controller.py
@@ -4,28 +4,44 @@ import vue
 import numpy
 import matplotlib.path as mpltPath
 
-# functio to transform the coordinates of a shape to be displayed at a certain
+# function to transform the coordinates of a shape to be displayed at a certain
 # locolisation
 # input : shape[[x,y]...],
 # top left hand corner coordinates where it's displayed [x,y]
 # output : new shape with offset
 def offsetShape (shape, offset) :
-    for i in range(0, len(shape)) :
-        shape[i][0] += offset[0]
-        shape[i][1] += offset[1]
-    return shape
+    localShape = numpy.copy(shape)
+    for i in range(0, len(localShape)) :
+        localShape[i][0] += offset[0]
+        localShape[i][1] += offset[1]
+    return localShape
 
-def shapeFits (shape) :    
-    path = mpltPath.Path(model.PATRON)
-    path.contains_points( shape, radius = 1.0 )
-    return 0
+# function to fin all the place where the shape can fit in the patron
+# input : shape you want to test + patron
+# output : list of valid coordinates
+def shapeFits (shape, patron) :
+    listResult = []
+    for i in range(0, len(patron)) :
+        validated = True
+        shapeOffset = offsetShape( shape, patron[i] )
+        path = mpltPath.Path( patron )
+        result = path.contains_points( shapeOffset, radius = 1.0 )
+        #print(result)
+        for y in range(0, len(result)) :
+            if not result[y] :
+                validated = False
+                break
+        if validated:
+            listResult.append(patron[i])
+    #print(listResult)
+    return listResult
 
 #_________________________________________________TEST_________________________________________________
 
 
 #_________________________________________________DISPLAY_________________________________________________
 
-shapeFits(model.SHAPE_1)
+shapeFits(model.SHAPE_1, model.PATRON)
 vue.affiche()
 
 
